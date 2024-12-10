b484511134f7fe0509a124af6dfb3ae81e673555
chb777-a <chb_b@naver.com>
2024-12-10 18:00:03 +0900

    day02

--- TJ2024_backend/src/day02/Example5.java
+++ TJ2024_backend/src/day02/Example5.java
@@ -7,12 +7,14 @@ public class Example5 {
 	public static void main(String[] args) {
 		//[1]
 		Scanner scan = new Scanner(System.in);
-		int kr = scan.nextInt();
+		
 		System.out.println("국어 점수 입력");
-		int en = scan.nextInt();
+		int kr = scan.nextInt();
 		System.out.println("영어 점수 입력");
-		int ma = scan.nextInt();
+		int en = scan.nextInt();
 		System.out.println("수학 점수 입력");
+		int ma = scan.nextInt();
+		
 		
 		
 		
@@ -23,18 +25,17 @@ public class Example5 {
 		System.out.println("3과목 평균 : " + result2);
 		
 		//[2]
-		
-		double circle = scan.nextDouble();
 		System.out.println("반지름 입력 :");
+		double circle = scan.nextDouble();
 		
 		double result3 = circle*circle*3.14;
 		System.out.println("원넓이 : " + result3);
 		
 		//[3]
-		double num1 = scan.nextDouble();
 		System.out.println("앞 실수 입력");
-		double num2 = scan.nextDouble();
+		double num1 = scan.nextDouble();
 		System.out.println("뒤 실수 입력");
+		double num2 = scan.nextDouble();
 		double num3 = num1/num2*100;
 		System.out.println("값의 비율 : " + num3 + "%");
 		
@@ -63,7 +64,7 @@ public class Example5 {
 		int 만원권 = (금액 % 100000) / 10000;
 		int 천원권 = (금액 % 10000) / 1000;
 		
-		System.out.printf("십만원권 : "+십만원권+" 만원권 : "+만원권+" 천원권: "+천원권+" ");
+		System.out.printf("십만원권 : "+십만원권+" 만원권 : "+만원권+" 천원권: "+천원권+" \n");
 		
 		
 		//[8]

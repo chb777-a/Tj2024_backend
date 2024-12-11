f93bf8c46167547857396a0d5bc31de4024130b4
chb777-a <chb_b@naver.com>
2024-12-11 11:03:55 +0900

    day03 연산자

--- TJ2024_backend/src/day03/Example1.java
+++ TJ2024_backend/src/day03/Example1.java
@@ -29,6 +29,64 @@ public class Example1 {// 자바는 번역(컴파일)단위를 클래스 단위�
 		System.out.println(5.0 / 3.0); //1.666666666667 -> 나누기 결과
 		System.out.println((int)(5.0/3.0)); // 1.66666666666667 -> 강제타입 -> 정수 변환
 		
+		int math = 90; int eng = 70;
+		int total = math + eng; //총점
+		System.out.println(total); // 총점변수의 값을 출력
+		
+		double avg = total / 2.0; // 평균 구하기
+		System.out.println(avg); //평균변수의 값을 출력
+		
+		
+		//복습
+		int korScore2 = 100;
+		int mathScore2 = 90;
+		int engScore2 = 80;
+		int tatalScore2 = korScore2 + mathScore2 + engScore2;
+		double avgScore2 = tatalScore2 / 3.0; // totalScore2(int) / 3(int); => tatalScore2(int) / 3.0(double);
+		System.out.println(tatalScore2);
+		System.out.println(avgScore2);
+		
+		//[4] 증감연산자 , ++ -- , 주의할 점 : ;(세미클론/명령어단위) 기준으로 연산 우선순위를 정한다.
+		int num2 = 10;
+		int value2;
+		value2 = ++num2; // num2 값이 1씩 증가 후에 value2 변수에 대입한다.
+			System.out.println(value2); // num2 = 11 , value2 = 11
+		value2 = num2++; // value2변수에 기존 num2 값이 먼저 대입한 후에 1씩 증가한다.
+			System.out.println(value2); // value2 = 11 , num2 = 12
+		value2 = --num2; // num2 값이 1씩 감소한 후에 value2 변수에 대입한다.
+			System.out.println(value2); // num2 = 11 , value2 = 11
+		value2 = num2--; // value2변수에 기존 num2 값이 먼저 대입한 후에 1씩 감소한다.
+			System.out.println(value2); // value2 = 11 , num2 = 10;
+		
+		//복습
+		int gameScore = 150;
+		int lastScore = ++gameScore; // 변수앞에 ++이 있으므로 대입(=)보다 1증가가 우선이다.
+		System.out.println(lastScore); // 151
+		
+		int lastScore2 = --gameScore; // 변수앞에 --이 있으므로 대입(=)보다 1감소가 우선이다.
+		System.out.println(lastScore2); // 150
+			//*사칙연산의 우선순위에 따라 *곱하기 후 +더하기 =>9
+		int result1 = 3+3*2; System.out.println(result1);
+			//* 연산순서
+			// 3 * 2 = 6
+			// 3 + 6 = 9
+		
+		int x = 10;
+		int y = x-- +5+ --x;
+			//* 연산순서
+			//1. x-- + 5 = 10 + 5 => 15 [x는 +연산후 1차감] x = 9
+			//2. 15 + --x = 15 + 8 => 23 [x는 +연산전 1차감] x = 8
+		System.out.println(x); // 8
+		System.out.println(y); // 23
+		
+		//[5] 비교연산자 , > 초과 < 미만 >=이상 <=이하 ==같다 !=다르다 , 결과는 true or false
+		int myAge = 31;
+		boolean value = (myAge >= 31);
+		System.out.println(value); // true
+			//() 소괄호를 사용한 이유는 가독성 높이기 위해서 사용
+		
+		
+		
 	}
 
 }

06a4225d259a6df8a88e6d0b99ea2f4f82fc427b
tj-bu-702-16 <tj-bu-702-16@DESKTOP-CV0RPMM>
2024-12-17 12:41:17 +0900

    day07

---
+++ backend_chb/src/day07/Example3.java
@@ -0,0 +1,10 @@
+package day07;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ backend_chb/src/day07/Product.java
@@ -0,0 +1,10 @@
+package day07;
+//(2) 제품 객체 만들기 위한 설계 클래스
+public class Product {
+	//+ 멤버변수 , 객체가 가지는 고유한 성질 / 값
+	int stock; // 재고 설정
+	int price; // 가격 설정
+	String name; // 제품명 설정
+	int cart; // 담은 수량 설정
+	
+}

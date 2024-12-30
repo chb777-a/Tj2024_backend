6554a5844854ccd8a9fe08942322ac470021b44f
chb777-a <chb_b@naver.cm>
2024-12-30 15:36:14 +0900

    Day15_상속_데이터베이스

--- backend_chb/src/database/2_데이터베이스생성삭제.sql
+++ backend_chb/src/database/2_데이터베이스생성삭제.sql
@@ -46,3 +46,46 @@ show variables like 'datadir';	-- 4. 로컬PC에 데이터베이스가 만들어
 use test1;						-- 5. 'test1' 이름의 데이터베이스 사용 활성화 하기 SQL 작성 
 
 # 참고 : 현재 작성한 코드 파일을 sql확장자 파일 , ctrl+s
+
+create database my1230;
+use my1230;
+#[1] 테이블을 생성한다. create table 테이블명(필드명);
+create table testtable1(필드명 int);
+#[2] 현재 활성화(사용) 중인 DB내 모든 테이블 조회ㅣ
+show tables;
+#[3] 테이블 생성한다. create table 테이블명 (필드명 타입 , 필드명 타입)
+#여러 개 필드가 존재할 경우 , (쉼표)로 구분한다.
+create table testtable2( 필드명1 int , 필드명2 double );
+#[4] 테이블 삭제한다. drop table 테이블명;
+drop table testtable1;
+drop table if exists testtable2;
+#[5] MYSQL데이터 타입
+/*
+	[정수]
+		tinyint		1바이트	-128~+127	vs byte
+        smallint	2바이트	+-3만 정도	vs short
+        mediumint	3바이트	+-800만 정도
+        int			4바이트	+21억 이상
+        bingint		8바이트	+-21억 이상	vs long
+        
+	[실수]
+		float		4바이트	부동소수점(표현 범위 이상일 경우에 오차 존재)
+        double		8바이트	부동소수점(표현 범위 이상일 경우에 오차 존재)
+        decimal		문자타입	실수를 문자로 처리하는 타입(오차 없음)
+	
+    [날짜/시간]
+		date		0000-00-00	,	1000년 ~ 9999년
+        time		시간표현 , 00:00:00
+        datetime	날짜/시간표현 , 0000-00-00 00:00:00
+        
+	[문자열]
+		char(문자길이)	:		문자열 표현 , 고정길이 , 최대 255글자 , char(5)		--> "유재석"	--->[유] [재] [석] [ ] [ ] 2칸 남음
+        varchar(문자길이)	:		문자열 표현 , 가변길이 , 최대 255글자 , varchar(5)	--> "유재석"	--->[유] [재] [석] 남은 길이는 제거
+        text			:		문자열 표현 , 고정길이 , 최대 6만글자 
+        longtext		:		문자열 표현 , 최대 42억개 글자 , 4GB , 대용량
+        
+	[논리]
+		bool	:	true or false 표현 , tinyint와 같다.
+        
+        
+        */

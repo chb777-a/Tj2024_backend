02835913fda47a53bb5ff54fa9122ba87d2b274a
chb777-a <chb_b@naver.cm>
2025-01-02 15:36:01 +0900

    Day17_인터페이스

---
+++ backend_chb/src/day17/step4/Customer.java
@@ -0,0 +1,21 @@
+package day17.step4;
+
+public class Customer implements Buy , Sell {
+	// 상속 : extends 클래스명
+		// 클래스 1개당 상속은 1번
+	// 구현 : implements 인터페이스명A , 인터페이스명B
+		// 클래스 1개당 구현은 여러개의 인터페이스
+	
+	// 두 인터페이스의 추상메소드 구체화/구현하기
+	@Override
+	public void sell() {
+		System.out.println("판매");
+		
+	}
+	@Override
+	public void buy() {
+		System.out.println("구매");
+		
+	}
+	
+}
